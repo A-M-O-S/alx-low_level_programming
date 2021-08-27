cb9e64608dcd2649774078cc86d0dafa46deffb5
A-M-O-S <ogwalja10@gmail.com>
2021-08-27 00:32:38 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ * Description: prints the reverse of lower case alphabets
+ * Return: Always (0) success
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a= 'z'; a>= 'a'; a--)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
+
