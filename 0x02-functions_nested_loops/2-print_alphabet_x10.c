be5ae69c9aeaf6b832c5114f4ba5fcd4cdddd472
A-M-O-S <ogwalja10@gmail.com>
2021-08-30 16:14:41 -0700

    10X alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include <holberton.h>
+/**
+ * main - Entrey point
+ * Description: make alphabet x10 times
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int i;
+
+	while (i <= 9)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+
+		_putchar('\n');
+		i++;
+	}
+}
+
