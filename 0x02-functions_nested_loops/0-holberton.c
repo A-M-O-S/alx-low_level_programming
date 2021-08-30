5ece1d0e4678e5d30b870b942685549f260a4353
A-M-O-S <ogwalja10@gmail.com>
2021-08-30 15:50:16 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,23 @@
+#include <holberton.h>
+
+/**
+ * main- Entry point
+ * Description: puts holberton \n
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	char str[] = "Holberton\n";
+	int i = 0;
+
+	while (str[i] != '\0'
+		{
+		char c = str[i];
+
+		_putchar(c);
+		i++;
+		}
+		return (0);
+		}
+
