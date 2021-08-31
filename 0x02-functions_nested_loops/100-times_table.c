3827d4cdfd1781135e8e7ae68cbea5a9d6384ee5
A-M-O-S <88572088+A-M-O-S@users.noreply.github.com>
2021-08-31 14:47:17 +0300

    100 times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,13 +1,15 @@
 #include "main.h"
+
 /**
- * prints_times_table - Prints a multiplication table up to param
- * @n: the number to be treated
+ * print_times_table - Prints a multiplication table up to param
+ * @n: The number to be treated
+ *
  * Return: Number matrix
  */
-
 void print_times_table(int n)
 {
-	int x,y,n;
+	int x, y, z;
+
 	if (n >= 0 && n <= 14)
 	{
 		for (x = 0; x <= n; x++)
@@ -47,5 +49,3 @@ void print_times_table(int n)
 		}
 	}
 }
-
-
